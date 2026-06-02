1833fa263fe62ef2576327bd3054294c4d9ab9c9
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:25:52 +0300

    if

---
+++ 02.06.2026.py
@@ -0,0 +1,26 @@
+# # разложение числа на рвзряды
+# num1=int(input("введите число "))
+# # d1=num1%10
+# # d2=num1//10%10
+# # d3=num1//100%10
+# # d4=num1//1000
+# # print(d1*1000+d2*100+d3*10+d4)
+# if num1>0:
+#     print(">0")
+#     print("конец программы")
+#
+#     print("конец условия")
+#
+# if num1%2==0
+#     print('четное')
+#     ifnum1%2=/=0
+#     print('нечетное')
+#     if
+#     print('num1')
+#     if num1>num2
+#         print(num1)
+#     else:
+#         print(num2)
+name=input("введите имя")
+
+
