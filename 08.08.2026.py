486e95ce6816e0af8b556394833eb85dc4db2a25
Взрослая академия <georgygevorkov@gmail.com>
2026-06-08 21:14:37 +0300

    циклы

---
+++ 08.08.2026.py
@@ -0,0 +1,33 @@
+
+# a=int(input())
+# b=int(input())
+# if a<b:
+#     a,b=b,a
+# while a>=b:
+#     print(a, end=" ")
+#     a=a-1
+# a=int(input())
+# b=int(input())
+# if a<b:
+#     a,b=b,a
+# while a>=b:
+#     print(a, end=" ")
+# a=int(input())
+# b=int(input())
+# if a>b: a,b=b,a
+# ifa%2: a+=1
+# while a<=b
+#     print(a)
+#     a+=2
+# a=int(input())
+# n=0
+# if n>0:
+#
+# a=6
+# n=0
+# while n<=a:
+#     print(n, end=" ")
+#     n+=1
+n=int("сколько чисел хотите ввести?")
+s=0
+while n>0
